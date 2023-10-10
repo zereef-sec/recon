4cd7f42e37601c9f6698e3f4cbd12d97a8b6e4f2
zereef_sec <108139112+zereef-sec@users.noreply.github.com>
2023-10-10 21:07:51 +0530

    Delete tools directory

--- tools/install.sh
+++
@@ -1,7 +0,0 @@
-go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-go install -v github.com/tomnomnom/anew@latest
-go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
-go install -v github.com/projectdiscovery/notify/cmd/notify@latest
-cp provider-config.yaml ~/.config/notify/provider-config.yaml
-git clone https://github.com/r3curs1v3-pr0xy/sub404.git;cd sub404;pip install -r ~/tools/sub404/requirements.txt
-go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
