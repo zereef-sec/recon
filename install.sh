5de0cf5b0c707a8fe95a3db83b7f6c266b18061c
zereef_sec <108139112+zereef-sec@users.noreply.github.com>
2023-10-10 21:24:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ go install -v github.com/projectdiscovery/notify/cmd/notify@latest
 cp provider-config.yaml ~/.config/notify/provider-config.yaml
 git clone https://github.com/r3curs1v3-pr0xy/sub404.git;cd sub404;pip install -r ~/tools/sub404/requirements.txt
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+go install github.com/lc/gau/v2/cmd/gau@latest
+go install github.com/projectdiscovery/katana/cmd/katana@latest
