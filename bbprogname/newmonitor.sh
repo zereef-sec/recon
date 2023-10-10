187f8718b7facf9907b55334fd624d072da9c91c
zereef_sec <108139112+zereef-sec@users.noreply.github.com>
2023-10-10 21:06:41 +0530

    Delete bbprogname directory

--- bbprogname/newmonitor.sh
+++
@@ -1,6 +0,0 @@
-while :
-do
-	subfinder -dL rootdomains.txt -all | anew subdomains.txt > newsubs.txt; echo "NEWSUBS:" | notify -id newsubs; notify -id newsubs -data newsubs.txt -bulk
-	httpx -l newsubs.txt -o newhttpx.txt -title -wc -sc cl -ct -location -web-server -asn; cat newhttpx.txt | cut -d " " -f 1 | cut -d "/" -f 3 > newalivesubs.txt;echo "NEWALIVESUBS:" | notify -id newsubs; notify -id newsubs -data newalivesubs.txt -bulk
-	sleep 3600
-done
--- bbprogname/oldmonitor.sh
+++
@@ -1,5 +0,0 @@
-do
-	httpx -l subdomains.txt -o httpx.txt -title -wc -sc cl -ct -location -web-server -asn; cat httpx.txt | cut -d " " -f 1 | cut -d "/" -f 3 > alivesubs.txt
-	cat httpx.txt | grep 404 | cut -d " " -f 1 | cut -d "/" -f 3 > 404subs.txt; echo "404SUBS:" | notify -id subtkovr; notify -id subtkovr -data 404subs.txt -bulk
-	python3 ~/tools/sub404/sub404.py --file alivesubs.txt | notify -id subtkovr -bulk
-done
