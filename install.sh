fedce042a3c66de201ecbb663c616f88d65ada98
zereefsec <108139112+zereefsec@users.noreply.github.com>
2024-06-05 20:28:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ cp provider-config.yaml ~/.config/notify/provider-config.yaml
 go install github.com/tomnomnom/waybackurls@latest
 apt install pipx -y
 pipx install git+https://github.com/xnl-h4ck3r/waymore.git
+pipx install bbot
 go install -v github.com/lc/gau/v2/cmd/gau@latest
 go install -v github.com/projectdiscovery/katana/cmd/katana@latest
 go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
