9d1a8497adfbd0c13447eaf4f42772add3c7e14e
zereefsec <108139112+zereefsec@users.noreply.github.com>
2024-06-05 13:46:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 # Install all the required tools
 apt update
 apt install golang -y
-apt install pipx -y
+export PATH=$PATH:~/go/bin
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 go install -v github.com/tomnomnom/anew@latest
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 go install -v github.com/projectdiscovery/notify/cmd/notify@latest
-cp provider-config.yaml ~/.config/notify/
+mkdir ~/.config/notify
+cp provider-config.yaml ~/.config/notify/provider-config.yaml
 go install github.com/tomnomnom/waybackurls@latest
+apt install pipx -y
 pipx install git+https://github.com/xnl-h4ck3r/waymore.git
 go install -v github.com/lc/gau/v2/cmd/gau@latest
 go install -v github.com/projectdiscovery/katana/cmd/katana@latest
